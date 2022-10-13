42c62dbb38fb4dcf66609df9f61a3dc8af72b446
Anthony Panarello <jpk8g2@gmail.com>
2022-10-13 14:40:04 -0400

    Changed player movement handling to access relavent Tilemap objects instead of TileController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,23 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
-using Unity.Jobs;
-using System;
+using UnityEngine.Tilemaps;
+
 
 public class PlayerController : MonoBehaviour
 {
-    public GameObject tileParentObject;
+    public Tilemap walls;
+    public Tilemap doors;
 
-    private int can_move = 6;
-    private TileController tc;
+    private int can_move;
+
     // Start is called before the first frame update
     void Start()
     {
-        this.transform.position = new Vector3(0, 0, -0.5f);
+        // Global game settings
         QualitySettings.vSyncCount = 0;
         Application.targetFrameRate = 60;
-        tc = tileParentObject.GetComponent<TileController>();
+
+        // Player Initialization
         this.GetComponent<SpriteRenderer>().material.color = new Color(0.8f, 0.4f, 0.2f, 1.0f);
+        this.transform.position = new Vector3(0, 0, -0.5f);
+        this.can_move = 6;
     }
 
     // Update is called once per frame
@@ -25,23 +31,36 @@ public class PlayerController : MonoBehaviour
     {
         if(can_move == 0) {
             handle_movement();
-            can_move = 4;
+            can_move = 5;
         }
         can_move--;
     }
 
+    // Handles Player Movement
     void handle_movement() {
-        int horizontal = (int)Input.GetAxisRaw("Horizontal");
-	    int vertical = (int)Input.GetAxisRaw("Vertical");
+        // Receive Input --------------------------------------------------
+        int horizontal = (int)Input.GetAxisRaw("Horizontal"); // -1, 0, 1
+	    int vertical = (int)Input.GetAxisRaw("Vertical");     // -1, 0, 1
+        // ----------------------------------------------------------------
 
+        // Create new position vector -------------------------------------
         Vector2 position = transform.position;
-	    position.x += horizontal;
-	    position.y += vertical;
-        
-        // Check if position is valid
 
-        if(tc.isTilePassable((int)position.x, (int)position.y)) {
+        // Prioritizes vertical movement
+        // Prevents movement on both axes simultaniously
+        if (vertical != 0) position.y += vertical;
+        else if (horizontal != 0) position.x += horizontal;
+        // ---------------------------------------------------------------
+        
+        // Check if position is valid --------------------------------------------------------------------
+        if (doors.GetTile(new Vector3Int((int)position.x - 1, (int)position.y - 1, 0))) {
+            // Handle player interaction with a door
+            Debug.Log($"Player Hit Door At Coords: ({(int)position.x - 1}, {(int)position.y - 1})");
+        }
+        else if (!walls.GetTile(new Vector3Int((int)position.x - 1, (int)position.y - 1, 0))) {
+            // Handle player movement across non-blocking (non-wall) objects
             GetComponent<Rigidbody2D>().position = position;
         }
+        // -----------------------------------------------------------------------------------------------
     }
 }
